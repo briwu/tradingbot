1c5694dbff4db006af8619dc80bc18bb277013de
Brian Wu <briawu@umich.edu>
2023-06-13 22:03:30 -0400

    begin signal generator implementation

--- bot.py
+++ bot.py
@@ -1,6 +1,14 @@
 import yfinance as yf
 import pandas as pd
 
-dataF = yf.download("EURUSD=X", start="2023-4-30", end="2023-6-10", interval='15m')
-print(dataF.iloc[:,:])
-#dataF.Open.iloc
+data = yf.download("EURUSD=X", start="2023-4-30", end="2023-6-10", interval='15m')
+print(data.iloc[:,:])
+data.Open.iloc
+
+def sig_generator(data):
+    curr_open = data.Open.iloc[-1]
+    curr_close = data.Close.iloc[-1]
+    prev_open = data.Open.iloc[-2]
+    prev_close = data.Open.iloc[-2]
+
+    
